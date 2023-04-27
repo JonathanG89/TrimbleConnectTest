83bee5e7e38e4f1e621463fa59752b85148df289
JonathanG89 <131447893+JonathanG89@users.noreply.github.com>
2023-04-27 14:58:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
  <script src="https://components.connect.trimble.com/trimble-connect-workspace-api/index.js"></script>
-  <div>
-    <button onclick="doConnect()">connect</button>  
-    <iframe hidden id="viewer" src="https://web.connect.trimble.com/?isEmbedded=true" width="100%" height="750px"/>
-    <script>
-        function doConnect(){
-          var viewer = document.getElementById("viewer");
-          viewer.hidden = false;
-          var API = await TrimbleConnectWorkspace.connect(viewer, (event, data) => {
-            console.log("Event: ", event, data);
-          });
-          API.embed.setTokens({accessToken: "587951a4-97a3-40bd-8039-b77e75d2e939"}); // or {shareToken: "shareToken_here"}
-          API.embed.init3DViewer({projectId: "kqafPqVOb7U"});
-        }
-    </script>
-  </div>
+    window.onload = (event) => {
+       getData();
+    };
+
+    async function getData() {
+     API = await TrimbleConnectWorkspace.connect(window.parent, (event, data) => {
+      console.log("Event: ", event, data);
+     });
+
+     API.project.getProject().then(project => {
+      console.log(project); // Trimble Connect project details
+     });
+    }
+ </script>
+
+ <script src="https://components.connect.trimble.com/trimble-connect-workspace-api/index.js"></script>
