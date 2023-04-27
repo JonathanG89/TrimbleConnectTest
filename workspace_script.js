523409342b775686512adfa4a2a509133cee067c
JonathanG89 <131447893+JonathanG89@users.noreply.github.com>
2023-04-27 15:22:08 +0200

    Create workspace_script.js

---
+++ workspace_script.js
@@ -0,0 +1,26 @@
+import * as WorkspaceAPI from "trimble-connect-workspace-api";
+
+this.API = await WorkspaceAPI.connect(
+  window.parent,
+  (event, args) => {
+    switch (event) {
+      case "extension.command":
+      //"Command executed by the user: args.data"
+        break;
+      case "extension.accessToken":
+      //"Accestoken or status: args.data"
+        break;
+      case "extension.userSettingsChanged":
+      //"User settings changed!"
+        break;
+      default:
+    }
+  },
+  30000
+);
+
+//Request for the access token.
+this.API.extension.getPermission("accesstoken").then((accessToken: string) => {
+    //Current user access token or status: accessToken
+    console.log(accesstoken);
+});
