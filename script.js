262f01499e22713921c1f77f576c2f11a61a13ae
JonathanG89 <131447893+JonathanG89@users.noreply.github.com>
2023-05-02 17:54:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
- <script src="https://components.connect.trimble.com/trimble-connect-workspace-api/index.js"></script>
-    
-  console.log("Test log");
+   console.log("Test log");
 
-    window.onload = (event) => {
-       getData();
-    };
+   var projectId;
+   var myAccessToken;
 
-    async function getData() {
-     API = await TrimbleConnectWorkspace.connect(window.parent, (event, data) => {
-      console.log("Event: ", event, data);
-     });
+   getData();
 
-     API.project.getProject().then(project => {
-      console.log(project); // Trimble Connect project details
-     });
-    }
- </script>
+   async function getData() {
+    API = await TrimbleConnectWorkspace.connect(window.parent, (event, data) => {
+       console.log("Event: ", event, data);
+    });
+
+    API.project.getProject().then(project => {
+       console.log(project); // Trimble Connect project details
+       projectId = project.id;
+    });
+
+    //Request for the access token.
+    API.extension.requestPermission("accesstoken").then(accessToken => {
+       //Current user access token or status: accessToken
+       console.log(accessToken);
+myAccessToken = accessToken;    
+    });
+
+    const propertyName = 'Propriété Test';
+    const propertyValue = 'Test';
+    const filter = `properties/${propertyName} eq null`;
+    /*const filter = `properties/${propertyName} eq '${propertyValue}'`;*/
+    const apiUrl = `https://pset-api.connect.trimble.com/propertyset/v1/projects/${projectId}/propertyset-instances?filter=${encodeURIComponent(filter)}`;
+
+    fetch(apiUrl, {
+       method: 'GET',
+       headers: {
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${myAccessToken}`
+       },
+    })
+    .then(response => response.json())
+    .then(data => {
+       const propertySetInstances = data.data;
+       // Use the propertySetInstances array to access the entities that match the filter
+       console.log(propertySetInstances)
+    });
+
+   }
